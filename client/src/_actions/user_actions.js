6118acca0bf7970a1ce6280056968d207c80292f
franklyayo <149245900+franklyayo@users.noreply.github.com>
2026-03-12 16:24:18 +0100

    Change addToCart from GET to POST request

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -53,7 +53,7 @@ export function logoutUser(){
 }
 
 export function addToCart(_id){
-    const request = axios.get(`${USER_SERVER}/addToCart?productId=${_id}`)
+    const request = axios.post(`${USER_SERVER}/addToCart?productId=${_id}`)    
     .then(response => response.data);
 
     return {
