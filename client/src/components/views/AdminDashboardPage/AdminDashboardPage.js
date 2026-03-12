aae18d81b8d0c204145db6bf4651a6148d68b0cc
franklyayo <149245900+franklyayo@users.noreply.github.com>
2026-03-13 00:39:36 +0100

    Create AdminDashboardPage component for admin panel

---
+++ client/src/components/views/AdminDashboardPage/AdminDashboardPage.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { Typography, Card, Row, Col } from 'antd';
+import Axios from 'axios';
+
+const { Title } = Typography;
+
+function AdminDashboardPage() {
+    const [users, setUsers] = useState([]);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        // We will build these backend routes next!
+        // Axios.get('/api/users/getAllUsers').then(...)
+        // Axios.get('/api/product/getProducts').then(...)
+    }, []);
+
+    return (
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <Title level={2}>Admin Control Panel</Title>
+            </div>
+
+            <Row gutter={[16, 16]}>
+                <Col lg={8} md={12} xs={24}>
+                    <Card title="Total Users" bordered={true} style={{ textAlign: 'center' }}>
+                        <Title level={3}>{users.length}</Title>
+                    </Card>
+                </Col>
+                <Col lg={8} md={12} xs={24}>
+                    <Card title="Total Products" bordered={true} style={{ textAlign: 'center' }}>
+                        <Title level={3}>{products.length}</Title>
+                    </Card>
+                </Col>
+                <Col lg={8} md={12} xs={24}>
+                    <Card title="Total Sales" bordered={true} style={{ textAlign: 'center' }}>
+                        <Title level={3}>0</Title> {/* Placeholder for sales data */}
+                    </Card>
+                </Col>
+            </Row>
+
+            {/* In the future, we can add Ant Design <Table> components here to list the actual users and purchases */}
+            
+        </div>
+    );
+}
+
+export default AdminDashboardPage;
