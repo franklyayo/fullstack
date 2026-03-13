ef4a060b857860919868090916f1aa85d947928c
franklyayo <149245900+franklyayo@users.noreply.github.com>
2026-03-13 01:29:16 +0100

    Refactor RightMenu for admin check and cart safety
    Added JSX Fragment wrapper for admin menu items and safety check for cart length.

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -33,30 +33,32 @@ function RightMenu(props) {
     );
   } else {
     return (
-     <Menu mode={props.mode}>
-        {/* Only render this item if the user is an Admin */}
-       {/* The safety check: Make sure userData exists BEFORE checking isAdmin */}
+      <Menu mode={props.mode}>
+        {/* The safety check + JSX Fragment wrapper */}
         {user.userData && user.userData.isAdmin && (
-            <> {/* <-- ADD THIS OPENING FRAGMENT */}
+          <>
             <Menu.Item key="upload">
-                <a href="/product/upload">Upload</a>
+              <a href="/product/upload">Upload</a>
             </Menu.Item>
-         <Menu.Item key="dashboard">
-                <a href="/admin/dashboard">Dashboard</a>
-        </Menu.Item>
-         < /> {/* <-- ADD THIS CLOSING FRAGMENT */}
+            <Menu.Item key="dashboard">
+              <a href="/admin/dashboard">Dashboard</a>
+            </Menu.Item>
+          </>
         )}
-      
+
         <Menu.Item key="history">
           <a href="/history">History</a>
         </Menu.Item>
+        
         <Menu.Item key="cart">
-          <Badge count={user.userData && user.userData.cart.length}>
+          {/* Added a safety check for the cart length to prevent page crashes */}
+          <Badge count={user.userData && user.userData.cart ? user.userData.cart.length : 0}>
             <a href="/user/cart">
               <ShoppingCartOutlined style={{ fontSize: 30, marginBottom: 4 }} />
             </a>
           </Badge>
         </Menu.Item>
+        
         <Menu.Item key="logout">
           <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
